6422f7e417304f07f5f6d80aee066a290d1dbace
baizhehao <1065682449@qq.com>
2019-05-08 20:09:00 +0800

    ssss

---
+++ src/router/index.js
@@ -0,0 +1,29 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+Vue.use(Router)
+
+
+const router = new Router({
+  routes: [
+    {
+      path: '/',
+      name: 'HelloWorld',
+      component: HelloWorld,
+    }
+  ]
+  
+})
+
+
+
+
+
+
+
+
+
+
+
+
+export default router

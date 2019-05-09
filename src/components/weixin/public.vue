38159ec4d0f5047e8957b36db3c8210d8228ab02
Your Name <email@example.com>
2019-05-09 20:24:33 +0800

    汶浩然

---
+++ src/components/weixin/public.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="weixin">
+        sss
+    </div>
+</template>
+
+<script>
+export default {
+name:"weixin"
+}
+</script>
+
+<style scoped>
+.weixin{
+    width:100%;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -4,7 +4,6 @@ import Router from 'vue-router'
 
 import index from '@/pages/index'
 import login from '@/pages/login'
-
 Vue.use(Router)
 
 

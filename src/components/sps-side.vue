2144849cf222d8a3c88f56fff1d5adc8149b6335
nike <978523089@qq.com>
2019-05-11 20:48:44 +0800

    品牌列表

--- src/components/sps-side.vue
+++ src/components/sps-side.vue
@@ -280,129 +280,208 @@
 
 <style scoped>
 ::-webkit-scrollbar {display:none}
-  .layout{
-    border: 1px solid #d7dde4;
-    background: #f5f7f9;
-    position: relative;
-    border-radius: 4px;
-    overflow: hidden;
-  }
-  .layout-header-bar{
-    background: #fff;
-    box-shadow: 0 1px 1px rgba(0,0,0,.1);
-    height: 90px;
-    border-bottom: 1px solid #f6f6f6;
-  }
-  .layout-logo-left{
-    width: 90%;
-    height: 30px;
-    background: #5b6270;
-    border-radius: 3px;
-    margin: 15px auto;
-  }
-  .menu-icon{
-    transition: all .3s;
-  }
-  .rotate-icon{
-    transform: rotate(-90deg);
-  }
-  .menu-item span{
-    display: inline-block;
-    overflow: hidden;
-    width:120px;
-    text-overflow: ellipsis;
-    white-space:nowrap;
-    vertical-align: bottom;
-    transition: width .2s ease .2s;
-  }
-  .menu-item i{
-    transform: translateX(0px);
-    transition: font-size .2s ease, transform .2s ease;
-    vertical-align: middle;
-    font-size: 16px;
-  }
-  .collapsed-menu span{
-    width: 0px;
-    transition: width .2s ease;
-  }
-  .collapsed-menu i{
-    transform: translateX(5px);
-    transition: font-size .2s ease .2s, transform .2s ease .2s;
-    vertical-align: middle;
-    font-size: 22px;
-  }
-  .ivu-menu-submenu-title{
-    padding: 14px 22px;
-  }
-  .ivu-dropdown{
-    position:relative;
-  }
-  .side_box{
-      display: flex;
-      flex-direction: column;
-  }
-  .side_tit{
-      height: 49px;
-      background:#FF7159;
-  }
-  .side_list{
-    flex:1;
-    overflow-y: auto;
-    background:#393D49;
-  }
-  .side_box span{
-    color: rgba(255,255,255,.8);
-    font-size: 16px;
-  }
-  .ivu-menu-dark.ivu-menu-vertical .ivu-menu-item:hover{
-     background:#FF7159;
-  }
-  .MenuTextClass{
-    background:#FF7159 !important;
-  }
-  .Header_left{
-    height:50px;
-    padding:0 10px;
-    display:flex;
-    justify-content:space-around;
-    align-items:center;
-  }
-  .Header_right{
+.layout{
+border: 1px solid #d7dde4;
+background: #f5f7f9;
+position: relative;
+border-radius: 4px;
+overflow: hidden;
+}
+.layout-header-bar{
+background: #fff;
+box-shadow: 0 1px 1px rgba(0,0,0,.1);
+height: 90px;
+border-bottom: 1px solid #f6f6f6;
+}
+.layout-logo-left{
+width: 90%;
+height: 30px;
+background: #5b6270;
+border-radius: 3px;
+margin: 15px auto;
+}
+.menu-icon{
+transition: all .3s;
+}
+.rotate-icon{
+transform: rotate(-90deg);
+}
+.menu-item span{
+display: inline-block;
+overflow: hidden;
+width:120px;
+text-overflow: ellipsis;
+white-space:nowrap;
+vertical-align: bottom;
+transition: width .2s ease .2s;
+}
+.menu-item i{
+transform: translateX(0px);
+transition: font-size .2s ease, transform .2s ease;
+vertical-align: middle;
+font-size: 16px;
+}
+.collapsed-menu span{
+width: 0px;
+transition: width .2s ease;
+}
+.collapsed-menu i{
+transform: translateX(5px);
+transition: font-size .2s ease .2s, transform .2s ease .2s;
+vertical-align: middle;
+font-size: 22px;
+}
+.ivu-menu-submenu-title{
+padding: 14px 22px;
+}
+.ivu-dropdown{
+position:relative;
+}
+.side_box{
+display: flex;
+flex-direction: column;
+}
+.side_tit{
+height: 49px;
+background:#FF7159;
+}
+.side_list{
+flex:1;
+overflow-y: auto;
+background:#393D49;
+}
+.side_box span{
+color: rgba(255,255,255,.8);
+font-size: 16px;
+}
+.ivu-menu-dark.ivu-menu-vertical .ivu-menu-item:hover{
+background:#FF7159;
+}
+.MenuTextClass{
+background:#FF7159 !important;
+}
+.Header_left{
+height:50px;
+padding:0 10px;
+display:flex;
+justify-content:space-around;
+align-items:center;
+}
+.Header_right{
 
-  }
-  .iconfont{
-    margin:0 20px;
-  }
-  .demo{
-    font-weight:600;
-  }
-  .but{
-    padding-top:16px;
-  }
-  .exit{
-    border-top:1px solid #ccc;
-  }
-  .ul_top{
-    background:#20222A;
-    color:#fff;
-    font-size:16px;
-    padding:0 80px 0 20px;
-  }
-  .ul_bottom{
-    padding: 10px 20px;
-    line-height:2;
-  }
-  .color{
-    display:inline;
-  }
-  .color_box{
-    display:inline-block;
-    width:100px;
-    height:100px;
-    background:lightsalmon;
-  }
-  .color_box_blue{
-    background:lightskyblue;
-  }
-  ::-webkit-scrollbar {display:none}
+}
+.iconfont{
+margin:0 20px;
+}
+.demo{
+font-weight:600;
+}
+.but{
+padding-top:16px;
+}
+.exit{
+border-top:1px solid #ccc;
+}
+.ul_top{
+background:#20222A;
+color:#fff;
+font-size:16px;
+padding:0 80px 0 20px;
+}
+.ul_bottom{
+padding: 10px 20px;
+line-height:2;
+}
+.color{
+display:inline;
+}
+.color_box{
+display:inline-block;
+width:100px;
+height:100px;
+background:lightsalmon;
+}
+.color_box_blue{
+background:lightskyblue;
+}
+.color_box_blue{
+background:lightpink;
+}
+.Header_top{
+width:100%;
+height:50px;
+line-height: 50px;display:flex;
+justify-content:space-between;
+border-bottom:1px solid #fff;
+}
+.Header_bottom{
+height:40px;
+background:#fff;
+display:flex;
+justify-content:space-between;
+border-bottom:1px solid #ccc;
+box-sizing:border-box;
+}
+.Header_bottom_One{
+display:flex;
+}
+.iconfont_two{
+display:block;
+width:40px;
+height:100%;
+border:1px solid #f6f6f6;
+box-sizing:border-box;
+margin:0;
+font-size:20px;
+text-align:center;
+line-height:40px;
+}
+.table_page{
+display:flex;
+flex:1;
+}
+.table_son{
+display:block;
+height:auto;
+background:#f6f6f6;
+border-top:2px solid #000;
+box-sizing:border-box;
+font-size:14px;
+line-height:40px;
+padding:0 40px 0 15px;
+border-right: 1px solid #f6f6f6;
+position:relative;
+}
+.call{
+position:absolute;
+right: 8px;
+top: 30%;
+font-size:16px;
+border-radius:50%;
+}
+.call:hover{
+background:#FF5722;
+color:#fff;
+}
+.Header_bottom_Two{
+display:flex;
+}
+.back{
+display:block;
+width:40px;
+height:auto;
+text-align:center;
+line-height:40px;
+}
+.iconfontBox{
+display:flex;
+}
+.iconfontSize{
+margin:0 15px 0 0;
+color:#fff;
+}
+.iconfontBtn{
+margin:0 15px 0 5px;
+font-weight:100;
+}
 </style>
